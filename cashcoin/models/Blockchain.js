048d6fbc929684b9f5b4dfec8ff19b973dd7c028
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-05-04 15:19:51 +0200

    fixed validation

--- cashcoin/models/Blockchain.js
+++ cashcoin/models/Blockchain.js
@@ -4,4 +4,16 @@ class Blockchain {
     constructor() {
         this.chain = [Block.genesis()];
     }
+
+    validateBlock(i) {
+        if (i < 0 || i >= this.chain.length) {
+            return false;
+        } else if (i === 0) {
+            return true
+        } else if (this.chain[i].previousHash !== this.chain[i - 1].hash) {
+            return false;
+        }
+
+        return true;
+    }
 }
