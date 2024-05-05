e0f0591aeabb74c72e827b9b4529afa49ae1ee9d
alexander-astrand <alexander.astrand@gmail.com>
2024-05-05 17:53:15 +0200

    Fixed add block errors and calculate hash correctly

--- cashcoin/controllers/addBlockController.js
+++ cashcoin/controllers/addBlockController.js
@@ -3,7 +3,7 @@ const cashCoin = require('../utilities/starter');
 exports.addBlock = (req, res) => {
     const block = req.body.block;
 
-    cashCoin.chain.push(block);
+    cashCoin.addBlock(block)
 
     res.status(201).json({ success: true, data: block });
 };
--- cashcoin/models/Blockchain.js
+++ cashcoin/models/Blockchain.js
@@ -19,6 +19,19 @@ class Blockchain {
     getLastBlock() {
         return this.chain.at(-1);
     }
+
+    addBlock(data) {
+        const previousBlock = this.getLastBlock()
+        const timestamp = Date.now();
+        const nonce = 0;
+
+        const hash = Block.hashBlock(nonce, timestamp, data, previousBlock.hash);
+
+        const newBlock = new Block(nonce, timestamp, data, previousBlock.hash);
+        newBlock.hash = hash;
+
+        this.chain.push(newBlock);
+    }
 }
 
 module.exports = Blockchain;
